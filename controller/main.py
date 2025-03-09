c8c6591f682185c3d011138d063dbe18d430de4e
ThiveyaSellar <sellarthiveya@gmail.com>
2025-03-09 16:07:50 +0100

    Inscription et Connexion de l'utilisateur avec JWT

--- controller/main.py
+++ controller/main.py
@@ -1,30 +1,19 @@
-import configparser, click, datetime, jwt, os, platform
+import configparser, click, jwt, os, sys
+
 from sqlalchemy import create_engine, text, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.exc import NoResultFound
-from datetime import datetime, timezone, timedelta
 
-from argon2 import PasswordHasher
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from models import User, Team
+# Ajouter le répertoire racine au sys.path
+from views.cli import display_login_registration_menu
+from models.models import User, Team
+from utils.token_utils import get_netrc_path, get_tokens_from_netrc, \
+    update_tokens_in_netrc, get_user_from_access_token
+from utils.token_utils import generate_tokens, is_token_expired
+from utils.db_utils import create_database_if_not_existent
+from views.cli import cli  # Importer la fonction cli
 
-def create_database(db_user, db_password, db_host, db_name):
-    # Créer objet de connexion sans nom de base de données pour vérifier son existence
-    engine = create_engine(
-        f"mysql+pymysql://{db_user}:{db_password}@{db_host}",
-        echo=False
-    )
-    conn = engine.connect()
-
-    # Vérifier si la base de données existe, sinon la créer
-    result = conn.execute(text(f"SHOW DATABASES LIKE '{db_name}'"))
-    if not result.fetchone():  # Si la base de données n'existe pas
-        print(f"La base de données {db_name} n'existe pas. Création...")
-        conn.execute(text(f"CREATE DATABASE {db_name}"))
-    else:
-        print(f"La base de données {db_name} existe déjà.")
-
-    conn.close()
 
 # Récupérer les infos de connexion à la base de données depuis le fichier config.ini
 config = configparser.ConfigParser()
@@ -35,7 +24,7 @@ db_password = config['database']['password']
 db_host = config['database']['host']
 db_name = config['database']['dbname']
 
-create_database(db_user, db_password, db_host, db_name)
+# create_database_if_not_existent(db_user, db_password, db_host, db_name)
 
 # Créer objet de connexion à la base de données
 engine = create_engine(
@@ -57,183 +46,37 @@ Base = declarative_base()
 # Créer les tables
 Base.metadata.create_all(engine)
 
-#------------------------------------------------------------
-    # JWT
-#------------------------------------------------------------
-
-"""user = User(name='John Snow', password='johnspassword')
-session.add(user)
-session.commit()
-
-print(user.id)  # None
-query = session.query(User).filter_by(name="John Snow")
-print(query.count())"""
-
 # Clé secrète pour signer le jeton JWT
 SECRET_KEY = "nvlzhvgi476hcich90796"
 
-#------------------------------------------------------------
-    # Netrc
-#------------------------------------------------------------
+"""
+    # -----------------------------------------------------------------------
+"""
 
-def is_token_expired(token):
-    try:
-        payload = jwt.decode(token, options={"verify_signature": False})
-        exp = payload.get("exp")
-        if exp:
-            expiration = datetime.fromtimestamp(exp, tz=timezone.utc)
-            return expiration < datetime.now(timezone.utc)
-        return True
-    except jwt.DecodeError:
-        return True
-
-def get_netrc_path():
-    # Afficher le système d'exploitation
-    os_name = platform.system()
-
-    if os_name == "Windows":
-        netrc_path = os.path.join(os.environ["USERPROFILE"], ".netrc")
-    else:
-        # netrc_path = os.path.join(os.environ["HOME"], ".netrc")
-        pass
-    return netrc_path
-
-def get_tokens_from_netrc(machine, netrc_path):
-    # Le fichier n'existe pas
-    if not os.path.exists(netrc_path):
-        return None, None
-
-    # Lire le contenu du fichier
-    with open(netrc_path, "r") as file:
-        lines = file.readlines()
-
-    machine_index = None
-    for i, line in enumerate(lines):
-        if line.strip() == f"machine {machine}":
-            machine_index = i
-            break
-
-    if machine_index is not None:
-        access_token, refresh_token = None, None
-        i = machine_index + 1
-
-        while i < len(lines) and not lines[i].startswith("machine "):
-            if lines[i].strip().startswith("access-token"):
-                access_token = lines[i].split()[1]
-            elif lines[i].strip().startswith("refresh-token"):
-                refresh_token = lines[i].split()[1]
-            i += 1
-
-        return access_token, refresh_token
-
-    return None, None
-
-def update_tokens_in_netrc(machine, access_token, refresh_token, netrc_path):
-    # Lire le contenu du fichier s'il existe
-    lines = []
-    if os.path.exists(netrc_path):
-        with open(netrc_path, "r") as file:
-            lines = file.readlines()
-
-    # Chercher la section de la machine
-    machine_index = None
-    for i, line in enumerate(lines):
-        if line.strip() == f"machine {machine}":
-            machine_index = i
-            break
-
-    if machine_index is not None:
-        # Si la machine existe, mettre à jour les tokens
-        i = machine_index + 1
-        access_updated, refresh_updated = False, False
-
-        while i < len(lines) and not lines[i].startswith("machine "):
-            if lines[i].strip().startswith("access-token"):
-                lines[i] = f"  access-token {access_token}\n"
-                access_updated = True
-            elif lines[i].strip().startswith("refresh-token"):
-                lines[i] = f"  refresh-token {refresh_token}\n"
-                refresh_updated = True
-            i += 1
-
-        # Ajouter les tokens manquants s'ils n'existaient pas
-        insert_index = machine_index + 1
-        if not access_updated:
-            lines.insert(insert_index, f"  access-token {access_token}\n")
-            insert_index += 1
-        if not refresh_updated:
-            lines.insert(insert_index, f"  refresh-token {refresh_token}\n")
-
-    else:
-        # ➕ Si la machine n'existe pas, l'ajouter avec les tokens
-        lines.extend([
-            f"\nmachine {machine}\n",
-            f"  access-token {access_token}\n",
-            f"  refresh-token {refresh_token}\n"
-        ])
-        print(f"Machine '{machine}' ajoutée avec ses tokens.")
-
-    # Écrire les modifications dans le fichier
-    with open(netrc_path, "w") as file:
-        file.writelines(lines)
-
-def get_token():
-    netrc_path = get_netrc_path()
-    access_token, refresh_token = get_tokens_from_netrc("127.0.0.1", netrc_path)
-    if access_token and refresh_token:
-        # Vérifier la validité des tokens
-        if is_token_expired(access_token):
-            return refresh_token
-        else:
-            return access_token
-    return None
-
-def generate_token(payload, secret_key):
-    token = jwt.encode(
-        payload=payload,
-        key=secret_key,
-        algorithm="HS256"
-    )
-    return token
-
-def generate_tokens(user):
-    payload_access = {
-        "user_id": user.id,
-        "email": user.email_address,
-        "role": user.team.name,
-        "exp": datetime.now() + timedelta(hours=3)
-    }
-    payload_refresh = {
-        "user_id": user.id,
-        "email": user.email_address,
-        "role": user.team.name,
-        "exp": datetime.now() + timedelta(days=30)
-    }
-    access_token = generate_token(payload_access, SECRET_KEY)
-    refresh_token = generate_token(payload_refresh, SECRET_KEY)
-    return access_token, refresh_token
-
-def create_netrc_file(machine, access_token, refresh_token, netrc_path):
-    with open(netrc_path, "w") as file:
-        file.write(f"machine {machine}\n")
-        file.write(f"access-token {access_token}\n")
-        file.write(f"refresh-token {refresh_token}\n")
-    print(f"Le fichier .netrc a été créé et les informations ajoutées à {netrc_path}")
+import datetime
+from datetime import datetime, timedelta
 
 
-#------------------------------------------------------------
-    # Click
-#------------------------------------------------------------
+import click, jwt, os
+
+from sqlalchemy.exc import NoResultFound
+
+from argon2 import PasswordHasher
+
+from models.models import Team, User
+from utils.token_utils import create_netrc_file, update_tokens_in_netrc, \
+    get_netrc_path
 
 @click.group()
 def cli():
     """Interface en ligne de commande"""
     pass
 
+
 @cli.command()
 def seed_teams():
     """Ajoute des équipes par défaut à la base de données."""
-    default_teams = ["Commercial","Gestion","Support"]
+    default_teams = ["Commercial", "Gestion", "Support"]
 
     for team_name in default_teams:
         if not session.query(Team).filter_by(name=team_name).first():
@@ -246,12 +89,13 @@ def seed_teams():
     session.commit()
     click.echo("Toutes les équipes par défaut ont été ajoutées.")
 
+
 @cli.command()
 @click.option("--email", prompt="Email", help="Votre email")
-@click.option("--password", prompt="Mot de passe", hide_input=True, help="Votre mot de passe")
+@click.option("--password", prompt="Mot de passe", hide_input=True,
+              help="Votre mot de passe")
 def login(email, password):
     """Commande de connexion"""
-
     try:
         # Vérification si le mail de l'utilisateur existe
         user = session.query(User).filter_by(email_address=email).one()
@@ -270,7 +114,8 @@ def login(email, password):
             "user_id": user.id,
             "email": user.email_address,
             "role": user.team.name,
-            "exp": datetime.now() + timedelta(hours=3)  # Expiration dans 1h
+            "exp": datetime.now() + timedelta(hours=1)
+            # Expiration dans 1h
         }
         access_token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
@@ -278,7 +123,7 @@ def login(email, password):
             "user_id": user.id,
             "email": user.email_address,
             "role": user.team.name,
-            "exp": datetime.now() + timedelta(days=30)
+            "exp": datetime.now() + timedelta(hours=3)
         }
         refresh_token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
@@ -293,7 +138,8 @@ def login(email, password):
         if not os.path.exists(netrc_path):
             create_netrc_file(machine, access_token, refresh_token, netrc_path)
         else:
-            update_tokens_in_netrc(machine, access_token, refresh_token, netrc_path)
+            update_tokens_in_netrc(machine, access_token, refresh_token,
+                                   netrc_path)
 
         # Enregistrer le refresh token dans la base de données
         user.token = refresh_token
@@ -305,19 +151,21 @@ def login(email, password):
     except NoResultFound:
         click.echo("Utilisateur introuvable.")
 
-# Commande d'inscription
+
 @cli.command()
 @click.option("--email", prompt="Email", help="Votre email")
 @click.option("--password", prompt="Mot de passe", hide_input=True,
               help="Votre mot de passe")
-@click.option("--password2", prompt="Confirmation de mot de passe", hide_input=True,
+@click.option("--password2", prompt="Confirmation de mot de passe",
+              hide_input=True,
               help="Votre mot de passe")
 @click.option("--first_name", prompt="Prénom", help="Votre prénom")
 @click.option("--last_name", prompt="Nom", help="Votre nom")
 @click.option("--phone", prompt="Numéro de téléphone", help="Votre téléphone")
-@click.option("--team", type=click.Choice(["Commercial","Gestion","Support"], case_sensitive=False), prompt="Équipe", help="Votre équipe")
+@click.option("--team", type=click.Choice(["Commercial", "Gestion", "Support"],
+                                          case_sensitive=False),
+              prompt="Équipe", help="Votre équipe")
 def register(email, password, password2, first_name, last_name, phone, team):
-    """Commande d'inscription"""
     if password != password2:
         click.echo(f"Les mots de passe ne matchent pas.")
         return
@@ -345,12 +193,8 @@ def register(email, password, password2, first_name, last_name, phone, team):
 
     click.echo(f"Inscription réussie pour {first_name} {last_name}.")
 
-def check_permissions(user, team):
-    if user.get("team") == team:
-        return True
-    return False
 
-def display_login_registration_menu():
+def display_login_registration_menu(session, SECRET_KEY):
     click.echo("Bienvenue !")
     choice = click.prompt(
         "Choisissez une option : 1- Connexion 2- Inscription", type=int)
@@ -365,54 +209,80 @@ def display_login_registration_menu():
     if choice == 1:
         # Connexion
         click.echo("Vous avez choisi de vous connecter.")
-        cli(["login"])  # Appel de la commande login
+        cli(["login"])
     elif choice == 2:
         # Inscription
         click.echo("Vous avez choisi de vous inscrire.")
-        cli(["register"])  # Appel de la commande register
+        cli(["register"])
+
+@cli.command()
+def seed_teams():
+
+    """Ajoute des équipes par défaut à la base de données."""
+    default_teams = ["Commercial","Gestion","Support"]
+
+    for team_name in default_teams:
+        if not session.query(Team).filter_by(name=team_name).first():
+            team = Team(name=team_name)
+            session.add(team)
+            click.echo(f"Équipe '{team_name}' ajoutée.")
+        else:
+            click.echo(f"Équipe '{team_name}' existe déjà.")
+
+    session.commit()
+    click.echo("Toutes les équipes par défaut ont été ajoutées.")
+
+"""
+    # -----------------------------------------------------------------------
+"""
 
 
-def get_user_from_access_token(access_token):
-    try:
-        payload = jwt.decode(access_token, SECRET_KEY, algorithms=["HS256"])
-        user_id = payload.get("user_id")
-        user = session.get(User, user_id)
-        return user
-    except jwt.ExpiredSignatureError:
-        click.echo("Token expiré.")
-        return None
-    except jwt.InvalidTokenError:
-        click.echo("Token invalide.")
-        return None
+def check_permissions(user, team):
+    if user.get("team") == team:
+        return True
+    return False
+
 
 
 def main():
-    # Récupérer dans le fichier .netrc les tokens
+
     expired_session = False
     netrc_path = get_netrc_path()
     machine = "127.0.0.1"
+    # Récupérer les tokens dans le fichier .netrc
     access_token, refresh_token = get_tokens_from_netrc(machine, netrc_path)
-    if access_token and refresh_token:
-        # Récupérer les informations de l'utilisateur
-        user = get_user_from_access_token(access_token)
-        if is_token_expired(access_token):
-            print("Access token expiré.")
-            if not is_token_expired(refresh_token):
-                print("Refresh token valide.")
-                # Générer un nouveau access token
-                access_token, refresh_token = generate_tokens(user)
-                update_tokens_in_netrc(machine, access_token, refresh_token, netrc_path)
-            else:
-                expired_session = True
-
-    if expired_session:
-        click.echo("Session expirée.")
-        display_login_registration_menu()
+    if not access_token and refresh_token:
+        # Inscription ou première connexion
+        display_login_registration_menu(session, SECRET_KEY)
     else:
-        click.echo(f"Bienvenue {user.first_name} !")
-        print(user.team.name)
-        print("Continuer les requêtes.")
+        # Récupérer les informations de l'utilisateur deouis le jeton d'accès
+        user = get_user_from_access_token(
+            refresh_token,
+            SECRET_KEY,
+            session
+        )
 
+        if is_token_expired(access_token):
+            if is_token_expired(refresh_token):
+                expired_session = True
+            else:
+                # Générer un nouveau access token
+                access_token, refresh_token = generate_tokens(user, SECRET_KEY)
+                update_tokens_in_netrc(
+                    machine, access_token, refresh_token, netrc_path
+                )
+
+        if expired_session:
+            click.echo("Session expirée.")
+            display_login_registration_menu(session, SECRET_KEY)
+        else:
+            click.echo(f"Bienvenue {user.first_name} !")
+            if user.team.name == "Commercial":
+                click.echo("Vous êtes dans l'équipe commerciale.")
+            elif user.team.name == "Gestion":
+                click.echo("Vous êtes dans l'équipe de gestion.")
+            elif user.team.name == "Support":
+                click.echo("Vous êtes dans l'équipe de support.")
 
 if __name__ == "__main__":
     # cli()
---
+++ utils/token_utils.py
@@ -0,0 +1,163 @@
+import click, jwt, os, platform
+from datetime import datetime, timezone, timedelta
+
+from models.models import User
+
+def is_token_expired(token):
+    try:
+        payload = jwt.decode(token, options={"verify_signature": False})
+        exp = payload.get("exp")
+        if exp:
+            expiration = datetime.fromtimestamp(exp, tz=timezone.utc)
+            return expiration.replace(tzinfo=None) < datetime.now()
+        return True
+    except jwt.DecodeError:
+        return True
+
+def get_netrc_path():
+    # Afficher le système d'exploitation
+    os_name = platform.system()
+
+    if os_name == "Windows":
+        netrc_path = os.path.join(os.environ["USERPROFILE"], ".netrc")
+    else:
+        # netrc_path = os.path.join(os.environ["HOME"], ".netrc")
+        pass
+    return netrc_path
+
+def get_tokens_from_netrc(machine, netrc_path):
+    # Le fichier n'existe pas
+    if not os.path.exists(netrc_path):
+        return None, None
+
+    # Lire le contenu du fichier
+    with open(netrc_path, "r") as file:
+        lines = file.readlines()
+
+    machine_index = None
+    for i, line in enumerate(lines):
+        if line.strip() == f"machine {machine}":
+            machine_index = i
+            break
+
+    if machine_index is not None:
+        access_token, refresh_token = None, None
+        i = machine_index + 1
+
+        while i < len(lines) and not lines[i].startswith("machine "):
+            if lines[i].strip().startswith("access-token"):
+                access_token = lines[i].split()[1]
+            elif lines[i].strip().startswith("refresh-token"):
+                refresh_token = lines[i].split()[1]
+            i += 1
+
+        return access_token, refresh_token
+
+    return None, None
+
+def update_tokens_in_netrc(machine, access_token, refresh_token, netrc_path):
+    # Lire le contenu du fichier s'il existe
+    lines = []
+    if os.path.exists(netrc_path):
+        with open(netrc_path, "r") as file:
+            lines = file.readlines()
+
+    # Chercher la section de la machine
+    machine_index = None
+    for i, line in enumerate(lines):
+        if line.strip() == f"machine {machine}":
+            machine_index = i
+            break
+
+    if machine_index is not None:
+        # Si la machine existe, mettre à jour les tokens
+        i = machine_index + 1
+        access_updated, refresh_updated = False, False
+
+        while i < len(lines) and not lines[i].startswith("machine "):
+            if lines[i].strip().startswith("access-token"):
+                lines[i] = f"  access-token {access_token}\n"
+                access_updated = True
+            elif lines[i].strip().startswith("refresh-token"):
+                lines[i] = f"  refresh-token {refresh_token}\n"
+                refresh_updated = True
+            i += 1
+
+        # Ajouter les tokens manquants s'ils n'existaient pas
+        insert_index = machine_index + 1
+        if not access_updated:
+            lines.insert(insert_index, f"  access-token {access_token}\n")
+            insert_index += 1
+        if not refresh_updated:
+            lines.insert(insert_index, f"  refresh-token {refresh_token}\n")
+
+    else:
+        # ➕ Si la machine n'existe pas, l'ajouter avec les tokens
+        lines.extend([
+            f"\nmachine {machine}\n",
+            f"  access-token {access_token}\n",
+            f"  refresh-token {refresh_token}\n"
+        ])
+        print(f"Machine '{machine}' ajoutée avec ses tokens.")
+
+    # Écrire les modifications dans le fichier
+    with open(netrc_path, "w") as file:
+        file.writelines(lines)
+
+def get_token():
+    netrc_path = get_netrc_path()
+    access_token, refresh_token = get_tokens_from_netrc("127.0.0.1", netrc_path)
+    if access_token and refresh_token:
+        # Vérifier la validité des tokens
+        if is_token_expired(access_token):
+            return refresh_token
+        else:
+            return access_token
+    return None
+
+def generate_token(payload, secret_key):
+    token = jwt.encode(
+        payload=payload,
+        key=secret_key,
+        algorithm="HS256"
+    )
+    return token
+
+def generate_tokens(user, SECRET_KEY):
+
+    payload_access = {
+        "user_id": user.id,
+        "email": user.email_address,
+        "role": user.team.name,
+        "exp": datetime.now() + timedelta(hours=1)
+    }
+    payload_refresh = {
+        "user_id": user.id,
+        "email": user.email_address,
+        "role": user.team.name,
+        "exp": datetime.now() + timedelta(hours=3)
+    }
+    access_token = generate_token(payload_access, SECRET_KEY)
+    refresh_token = generate_token(payload_refresh, SECRET_KEY)
+    return access_token, refresh_token
+
+def create_netrc_file(machine, access_token, refresh_token, netrc_path):
+    with open(netrc_path, "w") as file:
+        file.write(f"machine {machine}\n")
+        file.write(f"access-token {access_token}\n")
+        file.write(f"refresh-token {refresh_token}\n")
+    print(f"Le fichier .netrc a été créé et les informations ajoutées à {netrc_path}")
+
+def get_user_from_access_token(access_token, SECRET_KEY, session):
+    try:
+        payload = jwt.decode(access_token, SECRET_KEY, algorithms=["HS256"])
+        user_id = payload.get("user_id")
+        user = session.get(User, user_id)
+        return user
+    except jwt.ExpiredSignatureError:
+        click.echo("Token expiré.")
+        return None
+    except jwt.InvalidTokenError:
+        click.echo("Token invalide.")
+        return None
+
