a5d31bf017ea7868dffa702bb052d111f3684c6a
ThiveyaSellar <sellarthiveya@gmail.com>
2025-04-10 00:17:40 +0200

    Refactoring de la commande register dans le fichier

--- controller/menu.py
+++ controller/menu.py
@@ -13,7 +13,7 @@ from utils.token_utils import  create_netrc_file, get_netrc_path, \
 from models import User, Team, Event, Contract, Client
 
 # from MenuController import MenuController
-from LoginController2 import LoginController
+from LoginController import LoginController
 from views.MenuView import MenuView
 
 from settings import Settings
@@ -47,9 +47,57 @@ class MenuController:
                 click.echo(f"Erreur : {e}")
                 break
 
+class RegisterView:
+
+    def print_password_error(self):
+        click.echo(f"Les mots de passe ne matchent pas.")
+
+    def success_message(self, first_name, last_name):
+        click.echo(f"Inscription réussie pour {first_name} {last_name}.")
+
+
+class RegisterController:
+
+    def validate_password(self, password, password2):
+        return password == password2
+
+    def __hash_passwords(self, password):
+        ph = PasswordHasher()
+        return ph.hash(password)
+
+    def register(self, email, password, password2, first_name, last_name, phone, team):
+        registerView = RegisterView()
+
+        if not self.validate_password(password, password2):
+            registerView.print_password_error()
+            return
+
+        # Hachage du mot de passe
+        hashed_password = self.__hash_passwords(password)
+
+        # Récupérer l'id de l'équipe
+        team_id = session.query(Team).filter_by(name=team).one().id
+
+        # Création de l'utilisateur
+        new_user = User(
+            password=hashed_password,
+            first_name=first_name,
+            last_name=last_name,
+            email_address=email,
+            phone=phone,
+            team_id=team_id
+        )
+
+        # Enregistrement dans la base de données
+        session.add(new_user)
+        session.commit()
+
+        registerView.success_message(first_name, last_name)
+
 @click.group()
 def cli():
     pass
+
 
 @cli.command()
 @click.option("--email", prompt="Email", help="Votre email")
@@ -58,72 +106,25 @@ def login(email, password):
     controller = LoginController()
     controller.login(email, password, session, SECRET_KEY)
 
-"""@cli.command()
+@cli.command()
 @click.option("--email", prompt="Email", help="Votre email")
 @click.option("--password", prompt="Mot de passe", hide_input=True,
               help="Votre mot de passe")
-def login(email, password):
-    #Commande de connexion
-    # logique
-    print("A")
-    try:
-        # Vérification si le mail de l'utilisateur existe
-        user = session.query(User).filter_by(email_address=email).one()
-
-        # Permet d'hacher le mdp en version sécurisée et illisible
-        # Permet de comparer avec un mdp haché stocké en base de données
-        ph = PasswordHasher()
-        try:
-            print("B")
-            ph.verify(user.password, password)
-            print("C")
-        except:
-            # affichage
-            click.echo("Mot de passe incorrect.")
-            return
-        print("D")
-        # Génération des jetons d'accès et de rafraichissement JWT
-        payload = {
-            "user_id": user.id,
-            "email": user.email_address,
-            "role": user.team.name,
-            "exp": datetime.now() + timedelta(hours=1)
-            # Expiration dans 1h
-        }
-        access_token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
-        print("E")
-        payload = {
-            "user_id": user.id,
-            "email": user.email_address,
-            "role": user.team.name,
-            "exp": datetime.now() + timedelta(hours=3)
-        }
-        refresh_token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
-        print("F")
-        netrc_path = get_netrc_path()
-
-        # Stocker le jeton dans le fichier .netrc
-        # Définir le chemin du fichier .netrc dans le dossier utilisateur Windows
-
-        # Données à écrire dans .netrc
-        machine = "127.0.0.1"
-
-        if not os.path.exists(netrc_path):
-            create_netrc_file(machine, access_token, refresh_token, netrc_path)
-        else:
-            update_tokens_in_netrc(machine, access_token, refresh_token,
-                                   netrc_path)
-        # Enregistrer le refresh token dans la base de données
-        user.token = refresh_token
-        session.commit()
-
-        click.echo(f"Connexion réussie.")
-        click.echo(f"Bienvenue {user.first_name} {user.last_name}!")
-
-    except NoResultFound:
-        click.echo("Utilisateur introuvable.")
+@click.option("--password2", prompt="Confirmation de mot de passe",
+              hide_input=True,
+              help="Votre mot de passe")
+@click.option("--first_name", prompt="Prénom", help="Votre prénom")
+@click.option("--last_name", prompt="Nom", help="Votre nom")
+@click.option("--phone", prompt="Numéro de téléphone", help="Votre téléphone")
+@click.option("--team", type=click.Choice(["Commercial", "Gestion", "Support"],
+                                          case_sensitive=False),
+              prompt="Équipe", help="Votre équipe")
+def register(email, password, password2, first_name, last_name, phone, team):
+    controller = RegisterController()
+    controller.register(email, password, password2, first_name, last_name, phone, team)
 
 
+"""
 @cli.command()
 @click.option("--email", prompt="Email", help="Votre email")
 @click.option("--password", prompt="Mot de passe", hide_input=True,
@@ -164,6 +165,7 @@ def register(email, password, password2, first_name, last_name, phone, team):
     session.commit()
 
     click.echo(f"Inscription réussie pour {first_name} {last_name}.")
+"""
 
 @cli.command()
 def show_clients():
@@ -177,7 +179,8 @@ def show_events():
     events = session.query(Event).all()
     click.echo("------------- Events -------------")
     for event in events:
-        click.echo(f"{event.name} {event.end_date}")"""
+        click.echo(f"{event.name} {event.end_date}")
+
 
 def main():
 
