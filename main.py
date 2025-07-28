5e99c4e148ea54502dc696c257c9fa783eca080d
ThiveyaSellar <sellarthiveya@gmail.com>
2025-07-28 11:58:08 +0200

    Validation des champs lors de la saisie avec click

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, sys, click, jwt
+import os, sys, click, jwt, re
 
 # Ajouter le répertoire racine au sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '')))
@@ -19,27 +19,59 @@ settings = Settings()
 session = settings.session
 SECRET_KEY = settings.secret_key
 
+"""def email_exists_in_db(session, email):
+    session.query(Team).filter_by(name=team).one().id
+    """
+def validate_email(ctx, param, value):
+    email = value
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        raise click.BadParameter("L'email n'est pas valide.")
+    """ # Vérifier dans le cas d'une inscription si le mail existe déjà ou non
+    if ctx.command.name == "register":
+        if email_exists_in_db(session, email):
+            raise click.BadParameter("Cet email est déjà utilisé.")"""
+    return email
+
+def validate_phone(ctx, param, value):
+    phone = value
+    pattern = r"^0[1-9](\d{2}){4}$"
+    if not re.match(pattern, phone):
+        raise click.BadParameter("Le numéro n'est pas valide.")
+    return phone
+
+def validate_name(ctx, param, value):
+    name = value
+    if not re.match(r"^[A-Za-zÀ-ÖØ-öø-ÿ\-']+$", name):
+        if param.name == "last_name":
+            label = "Nom"
+        elif param.name == "first_name":
+            label = "Prénom"
+        else:
+            label = param.name.capitalize()
+        raise click.BadParameter(f"{label} invalide")
+    return name
+
 @click.group()
 def cli():
     pass
 
 @cli.command()
-@click.option("--email", prompt="Email", help="Votre email")
+@click.option("--email", prompt="Email", callback=validate_email, help="Votre email")
 @click.option("--password", prompt="Mot de passe", hide_input=True, help="Votre mot de passe")
 def login(email, password):
     controller = LoginController()
     controller.login(email, password, session, SECRET_KEY)
 
 @cli.command()
-@click.option("--email", prompt="Email", help="Votre email")
+@click.option("--email", prompt="Email", callback=validate_email, help="Votre email")
 @click.option("--password", prompt="Mot de passe", hide_input=True,
               help="Votre mot de passe")
 @click.option("--password2", prompt="Confirmation de mot de passe",
               hide_input=True,
               help="Votre mot de passe")
-@click.option("--first_name", prompt="Prénom", help="Votre prénom")
-@click.option("--last_name", prompt="Nom", help="Votre nom")
-@click.option("--phone", prompt="Numéro de téléphone", help="Votre téléphone")
+@click.option("--first_name", prompt="Prénom", callback=validate_name, help="Votre prénom")
+@click.option("--last_name", prompt="Nom", callback=validate_name, help="Votre nom")
+@click.option("--phone", prompt="Numéro de téléphone", callback=validate_phone, help="Votre téléphone")
 @click.option("--team", type=click.Choice(["Commercial", "Gestion", "Support"],
                                           case_sensitive=False),
               prompt="Équipe", help="Votre équipe")
